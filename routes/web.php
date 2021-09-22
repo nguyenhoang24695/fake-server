8d10002d6118ffe0673cbc9eeb3f948e9c4811bc
hoangnv <hoangnvd00536@fpt.edu.vn>
2021-09-22 10:34:25 +0700

    init

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return '1';
+});
+
+Route::get('/upload_plantix1', function (Request $request) {
+    return $request->getContent();
+});
+Route::post('/upload_plantix', function (Request $request) {
+//    dd($request->files);
+    $data = '';
+//    foreach($request->allFiles() as $key => $value){
+//        fwrite($fp, $key);
+//    }
+    foreach($request->all() as $key => $value){
+        $data = $data.$key;
+        $data = $data.":";
+        $data = $data.$value;
+        $data = $data."\n";
+    }
+    file_put_contents('./data.txt', $data.PHP_EOL , FILE_APPEND | LOCK_EX);
+    return $request->all();
+});
